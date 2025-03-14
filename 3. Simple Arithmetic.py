96595e77cd23f010a5c357d91199eced05ba1a88
VishwasSR07 <vishwas.s.raichur7@gmail.com>
2025-03-14 23:22:26 +0530

    Create 3. Simple Arithmetic.py

---
+++ 3. Simple Arithmetic.py
@@ -0,0 +1,4 @@
+# 3. Simple Arithmetic
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+print(f"Sum: {a + b}, Difference: {a - b}, Product: {a * b}, Quotient: {a / b}")
